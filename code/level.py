3c038bb980fe334dee48dd376cc38a3f227c5ade
Lucas <lucas@lucas.com>
2026-06-15 01:02:19 -0300

    Creating Mediator Pattern. Enemies destruction.

--- code/level.py
+++ code/level.py
@@ -7,6 +7,7 @@ import pygame
 from pygame.font import Font
 from pygame import Surface, Rect
 
+from code.entityMediator import EntityMediator
 from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENTY_ENEMY, SPAWN_TIME
 from code.entity import Entity
 from code.entityFactory import EntityFactory
@@ -47,6 +48,10 @@ class Level:
             self.level_text(14, f'{clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
             self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
+            # Collisions
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
+
         pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
